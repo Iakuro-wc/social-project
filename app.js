51690ee1d53950c5a8bcfd11a577bf446c79000f
orukai <wcdevcontent@gmail.com>
2018-05-06 19:54:06 -0700

    added a working register form with proper route handlers as well as a mini post on the main directory with the registration firstname. I also added a usage of body-parser on app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); //so that i don't have to render with .ejs
 
 app.get("/", (req, res) => {
@@ -11,16 +13,43 @@ app.get("/register", (req, res) => {
 	res.render("register");
 })
 
+const newUser = [
+	{
+		firstname: "will",
+		lastname: "bause",
+		email: "willbause@bausest.com",
+		password: "superbause",
+		country: "bause-nation"
+	}
+]
+
+//finish this first
 app.post("/register", (req, res) => {
+	let firstname = req.body.firstname;
+	let lastname = req.body.lastname;
+	let email = req.body.email;
+	let password = req.body.password;
+	let country = req.body.country;
+	const newestUser = {
+		firstname: firstname,
+		lastname: lastname,
+		email: email,
+		password: password,
+		country: country
+	}
+	newUser.push(newestUser);
+
 	res.redirect("main");
+	console.log(newUser);
 })
 
 app.post("/main", (req, res) => {
-
+	console.log(req.body);
 	res.redirect("/main");
 })
 app.get("/main", (req, res) => {
-	res.render("main");
+
+	res.render("main", {newUser: newUser});
 })
 
 app.listen(3000, () => {
